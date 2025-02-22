d0633329d39b5f94ebcd45085b9140325be60b9a
MohammedBambyql <mhmdbambyql123@gmail.com>
2025-02-21 19:23:12 -0800

    Setup Fonts and Assets

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,9 +10,7 @@ export default function Index() {
         alignItems: 'center',
       }}
     >
-      <Text className=" font-bold text-lg my-10 color-lime-400">
-        Welcome To REACT
-      </Text>
+      <Text className="text-3xl my-10 font-rubik ">Welcome to ReState</Text>
       <Link href="/sign-in">Sign in</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
